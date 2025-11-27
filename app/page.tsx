bc896a6ce4e9d479a90b717b74f21a2890f961ca
Tom Gumbuo <gumbuogw3@gmail.com>
2025-11-27 12:15:22 -0500

    feat: Enable sound for video autoplay

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
             <div className="relative rounded-xl overflow-hidden shadow-2xl border-4 border-cyan-500">
               <video
                 autoPlay
-                muted
                 controls
                 className="w-full h-auto"
                 onEnded={closeVideo}
